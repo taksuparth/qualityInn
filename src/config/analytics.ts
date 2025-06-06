d05963fccf3ff68e4b37fecab7308b67d5ad2ec0
Lalit Bafna <labafna@gmail.com>
2025-06-06 06:46:02 +0530

    Google tag manager code added

--- src/config/analytics.ts
+++ src/config/analytics.ts
@@ -5,4 +5,4 @@
 
 export const googleSiteVerification = ''
 export const googleAnalyticsMeasurementID = 'G-T0YHRXJT0J'
-export const googleTagManagerID = 'AW-17151002307'
+export const googleTagManagerID = 'GTM-MTSS2V87'
